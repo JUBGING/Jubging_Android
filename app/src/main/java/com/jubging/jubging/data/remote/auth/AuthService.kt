b316122cf76642adc6992a1d546bec980f7b0df0
hwanghs111 <hwanghs111@naver.com>
2022-11-03 18:38:21 +0900

    로그인 오류코드 정상적으로

--- app/src/main/java/com/jubging/jubging/data/remote/auth/AuthService.kt
+++ app/src/main/java/com/jubging/jubging/data/remote/auth/AuthService.kt
@@ -27,52 +27,27 @@ object AuthService {
         authService.signUp(user).enqueue(object : Callback<SignUpResponse> {
             override fun onResponse(call: Call<SignUpResponse>, response: Response<SignUpResponse>) {
 
-                when(response.code()){
-
-                    201->{
-                        val resp = response.body()!!
-                        signUpView.onSignUpSuccess()
+                if(response.code() == 201){
+                    val resp = response.body()!!
+                    signUpView.onSignUpSuccess()
+                }
+                else{
+                    var jsonObject: JSONObject? = null
+                    var errorDto : ErrorResponse? = null
+                    try{
+                        jsonObject = JSONObject(response.errorBody()!!.string())
+                        val errorCode = jsonObject.getInt("errorCode")
+                        val message = jsonObject.getString("message")
+                        errorDto = ErrorResponse(errorCode= errorCode, message)
+                    }
+                    catch (e: JSONException){
+                        e.printStackTrace()
                     }
 
-                    400->{
-                        var jsonObject: JSONObject? = null
-                        var errorDto : ErrorResponse? = null
-                        try{
-                            jsonObject = JSONObject(response.errorBody()!!.string())
-                            val errorCode = jsonObject.getInt("errorCode")
-                            val message = jsonObject.getString("message")
-                            errorDto = ErrorResponse(errorCode= errorCode, message)
-                        }
-                        catch (e: JSONException){
-                            e.printStackTrace()
-                        }
-
-                        if (errorDto != null) {
-                            signUpView.onSignUpFailure(errorDto.errorCode, errorDto.message)
-                        }
+                    if (errorDto != null) {
+                        signUpView.onSignUpFailure(errorDto.errorCode, errorDto.message)
                     }
                 }
-//                if(response.isSuccessful){
-//                    val resp = response.body()!!
-//                    signUpView.onSignUpSuccess()
-//                }
-//                else{
-//                    var jsonObject: JSONObject? = null
-//                    var errorDto : ErrorResponse? = null
-//                    try{
-//                        jsonObject = JSONObject(response.errorBody()!!.string())
-//                        val errorCode = jsonObject.getInt("errorCode")
-//                        val message = jsonObject.getString("message")
-//                        errorDto = ErrorResponse(errorCode= errorCode, message)
-//                    }
-//                    catch (e: JSONException){
-//                        e.printStackTrace()
-//                    }
-//
-//                    if (errorDto != null) {
-//                        signUpView.onSignUpFailure(errorDto.errorCode, errorDto.message)
-//                    }
-//                }
 
             }
 
@@ -91,54 +66,28 @@ object AuthService {
         authService.login(user).clone().enqueue(object : Callback<AuthResponse>{
             override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
                 Log.d("로그인", user.toString())
-                Log.d("응답", response.errorBody()?.string().toString())
 
-                when(response.code()){
-                    200 -> {
-                        val resp = response.body()!!
-                        loginView.onLoginSuccess(resp)
+                if(response.code() == 200){
+                    val resp = response.body()!!
+                    loginView.onLoginSuccess(resp)
+                }
+                else{
+                    var jsonObject: JSONObject? = null
+                    var errorDto : ErrorResponse? = null
+                    try{
+                        jsonObject = JSONObject(response.errorBody()!!.string())
+                        val errorCode = jsonObject.getInt("errorCode")
+                        val message = jsonObject.getString("message")
+                        errorDto = ErrorResponse(errorCode= errorCode, message)
+                    }
+                    catch (e: JSONException){
+                        e.printStackTrace()
                     }
 
-                    400 -> {
-                        var jsonObject: JSONObject? = null
-                        var errorDto : ErrorResponse? = null
-                        try{
-                            jsonObject = JSONObject(response.errorBody()!!.string())
-                            val errorCode = jsonObject.getInt("errorCode")
-                            val message = jsonObject.getString("message")
-                            errorDto = ErrorResponse(errorCode= errorCode, message)
-                        }
-                        catch (e: JSONException){
-                            e.printStackTrace()
-                        }
-
-                        if (errorDto != null) {
-                            loginView.onLoginFailure(errorDto.errorCode, errorDto.message)
-                        }
+                    if (errorDto != null) {
+                        loginView.onLoginFailure(errorDto.errorCode, errorDto.message)
                     }
                 }
-
-//                if(response.isSuccessful){
-//                    val resp = response.body()!!
-//                    loginView.onLoginSuccess(resp)
-//                }
-//                else{
-//                    var jsonObject: JSONObject? = null
-//                    var errorDto : ErrorResponse? = null
-//                    try{
-//                        jsonObject = JSONObject(response.errorBody()!!.string())
-//                        val errorCode = jsonObject.getInt("errorCode")
-//                        val message = jsonObject.getString("message")
-//                        errorDto = ErrorResponse(errorCode= errorCode, message)
-//                    }
-//                    catch (e: JSONException){
-//                        e.printStackTrace()
-//                    }
-//
-//                    if (errorDto != null) {
-//                        loginView.onLoginFailure(errorDto.errorCode, errorDto.message)
-//                    }
-//                }
          }
 
             override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
