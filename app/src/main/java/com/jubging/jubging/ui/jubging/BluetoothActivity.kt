f1a683589630ca9a6c6bb9e26c31e7aa4e43e498
99kenny <helloiamkenny@gmail.com>
2022-11-16 17:45:53 +0900

    hmm

--- app/src/main/java/com/jubging/jubging/ui/jubging/BluetoothActivity.kt
+++ app/src/main/java/com/jubging/jubging/ui/jubging/BluetoothActivity.kt
@@ -2,9 +2,10 @@ package com.jubging.jubging.ui.jubging
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,6 +14,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,10 +22,14 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.jubging.jubging.databinding.ActivityBluetoothBinding
+import kotlinx.android.synthetic.main.activity_bluetooth.*
+import java.io.IOException
+import kotlin.reflect.KProperty
 
 
 class BluetoothActivity: AppCompatActivity() {
     private var mBinding: ActivityBluetoothBinding? = null
+
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = mBinding!!
     val PERMISSIONS = arrayOf(
@@ -36,22 +42,24 @@ class BluetoothActivity: AppCompatActivity() {
     )
     val REQUEST_ALL_PERMISSION = 2
 
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private lateinit var broadcastReceiver: BroadcastReceiver
+
     private val activityResultLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            Log.d("TEST","블투 켜기 요청")
+            Log.d("TEST", "블투 켜기 요청")
             if (it.resultCode == RESULT_OK) {
-                Log.d("TEST","활성화")
+                Log.d("TEST", "활성화")
                 Toast.makeText(applicationContext, "활성화", Toast.LENGTH_SHORT).show()
             } else if (it.resultCode == RESULT_CANCELED) {
-                Log.d("TEST","비활성화")
-
+                Log.d("TEST", "비활성화")
                 Toast.makeText(applicationContext, "비활성화", Toast.LENGTH_SHORT).show()
             }
         }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //블루투스 어댑터 가져오기
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
         //블루투스 지원하지 않는 기기인 경우
         if (bluetoothAdapter == null) {
         }
@@ -115,60 +123,30 @@ class BluetoothActivity: AppCompatActivity() {
 //            }
 //        }
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
-            Log.d("TEST","안드로이드 12")
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.d("TEST", "안드로이드 12")
             if (!hasPermissions(this, PERMISSIONS_S_ABOVE)) {
-                Log.d("TEST","권한 요청")
-
+                Log.d("TEST", "권한 요청")
                 requestPermissions(PERMISSIONS_S_ABOVE, REQUEST_ALL_PERMISSION)
             }
-        }else{
+        } else {
             if (!hasPermissions(this, PERMISSIONS)) {
-                Log.d("TEST","권한요청")
-
+                Log.d("TEST", "권한요청")
                 requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
             }
         }
 
-        if(bluetoothAdapter?.isEnabled == false){
-            Log.d("TEST","블루투스 꺼져있음")
+        if (bluetoothAdapter?.isEnabled == false) {
+            Log.d("TEST", "블루투스 꺼져있음")
             val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             activityResultLauncher.launch(intent)
         }
-
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
 
         mBinding = ActivityBluetoothBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private val receiver = object : BroadcastReceiver() {
-
-        @SuppressLint("MissingPermission")
-        override fun onReceive(context: Context, intent: Intent) {
-            val action: String? = intent.action
-            when(action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    // Discovery has found a device. Get the BluetoothDevice
-                    // object and its info from the Intent.
-                    val device: BluetoothDevice? =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device?.name
-                    val deviceHardwareAddress = device?.address // MAC address
-                    if (deviceName != null) {
-                        Log.d("블루투스",deviceName)
-                    }
-                    else{
-                        Log.d("블루투스","null")
-                    }
-                }
-            }
-        }
-    }
-
+    //블루투스 권한
     private fun hasPermissions(context: Context, permissions: Array<String>): Boolean {
         for (permission in permissions) {
             if (ActivityCompat.checkSelfPermission(context, permission)
@@ -180,6 +158,7 @@ class BluetoothActivity: AppCompatActivity() {
         return true
     }
 
+    //권한요청 콜백
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -202,7 +181,7 @@ class BluetoothActivity: AppCompatActivity() {
 
     // 액티비티가 파괴될 때..
     override fun onDestroy() {
-        unregisterReceiver(receiver)
+        unregisterReceiver(broadcastReceiver)
         // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
         mBinding = null
         super.onDestroy()
