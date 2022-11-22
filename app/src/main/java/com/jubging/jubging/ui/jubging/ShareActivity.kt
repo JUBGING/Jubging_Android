b7316f3ef0d6c0c777f385e0539ecb74f3a21ede
dntjr8989 <dntjr8989@naver.com>
2022-11-22 19:07:27 +0900

    import 정리

--- app/src/main/java/com/jubging/jubging/ui/jubging/ShareActivity.kt
+++ app/src/main/java/com/jubging/jubging/ui/jubging/ShareActivity.kt
@@ -10,7 +10,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
